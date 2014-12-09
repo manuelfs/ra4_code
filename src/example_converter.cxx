f3bd0a14f596f3eea0b21b73a1622861cee12671
Adam Dishaw <aldishaw@gmail.com>
2014-12-09 14:26:49 -0800

    Produce example files with either cfA or configurableAnalysis subdirectory

--- src/example_converter.cxx
+++ src/example_converter.cxx
@@ -16,25 +16,40 @@ int main(int argc, char *argv[]){
     return EXIT_FAILURE;
   }
 
+  if(string(argv[1]) == string(argv[2])){
+    cerr << "Input and output file are the same: " << argv[1] << '.' << endl;
+    return EXIT_FAILURE;
+  }
+
   TFile in_file(argv[1], "read");
   if(in_file.IsZombie() || !in_file.IsOpen()){
     cerr << "Could not open input file " << argv[1] << '.' << endl;
     return EXIT_FAILURE;
   }
 
-  const string cfa = "cfA";
-  TTree *event_a = static_cast<TTree*>(in_file.Get((cfa+"/eventA").c_str()));
+  string cfa = "configurableAnalysis";
+  TDirectory *dir = static_cast<TDirectory*>(in_file.Get(cfa.c_str()));
+  if(!dir){
+    cfa = "cfA";
+    dir = static_cast<TDirectory*>(in_file.Get(cfa.c_str()));
+    if(!dir){
+      cerr << "File " << argv[1] << " has nither a cfA nor a configurableAnalysis TDirectory." << endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  TTree *event_a = static_cast<TTree*>(dir->Get("eventA"));
   if(!event_a){
-    cerr << "Could not find " << cfa << "/eventA in " << argv[1] << '.' << endl;
+    cerr << "Could not find eventA in subdirectory " << cfa << " in file " << argv[1] << '.' << endl;
     return EXIT_FAILURE;
   }
 
-  TTree *event_b = static_cast<TTree*>(in_file.Get((cfa+"/eventB").c_str()));
+  TTree *event_b = static_cast<TTree*>(dir->Get("eventB"));
   if(!event_b){
-    cerr << "Could not find " << cfa << "/eventB in " << argv[1] << '.' << endl;
+    cerr << "Could not find eventB in subdirectory " << cfa << " in file " << argv[1] << '.' << endl;
     return EXIT_FAILURE;
   }
-  
+
   TTree *event_a_out = event_a->CloneTree(0);
   if(!event_a_out){
     cerr << "Could not clone eventA." << endl;
@@ -50,6 +65,10 @@ int main(int argc, char *argv[]){
   TFile out_file(argv[2], "recreate");
   if(out_file.IsZombie() || !out_file.IsOpen()){
     cerr << "Could not open output file " << argv[2] << '.' << endl;
+    return EXIT_FAILURE;
+  }
+  if(!out_file.cd()){
+    cerr << "Could not cd into " << argv[2] << '.' << endl;
     return EXIT_FAILURE;
   }
 
@@ -68,7 +87,7 @@ int main(int argc, char *argv[]){
     cerr << "Could not write eventA." << endl;
     return EXIT_FAILURE;
   }
-  
+
   if(!event_b_out->Write()){
     cerr << "Could not write eventB." << endl;
     return EXIT_FAILURE;
