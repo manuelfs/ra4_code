929bbd62244dac04ff06c31b56e4445fc99bb37e
trandbea <akovcharova@gmail.com>
2015-05-11 22:16:55 -0700

    remove old files that don't compile with new trees

--- inc/loop_tree.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef H_LOOP_TREE
-#define H_LOOP_TREE
-
-#include "small_tree.hpp"
-#include <string>
-
-double passIsolationCut(const small_tree &tree, int ilep, bool isElectron, bool isveto, std::string isotype, const double coneiso_cut);
-
-#endif
